2b48455b40914cf60149d5d7274151acd7ef304f
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2014-02-10 17:17:20 +0900

    modify init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,25 +8,38 @@
 
 USER="phantomjs"
 APP="/usr/local/bot_handler/server.js"
-DAEMON="path_to/phantomjs $APP"
+DAEMON="path_to/phantomjs"
 LOG_FILE="/dev/null"
+ENVIRONMENT="production"
+PID="/var/run/phantomjs/phantomjs.pid"
+SLOCK="/var/lock/subsys/phantomjs"
 
 do_start()
 {
         echo -n $"Starting PhantomJS : "
-        runuser -l "$USER" -c "$DAEMON >> $LOG_FILE &" && echo_success || echo_failure
+        runuser -l "$USER" -c "PHANTOM_ENV=$ENVIRONMENT $DAEMON $APP >> $LOG_FILE &" && echo_success || echo_failure
         RETVAL=$?
-  echo
-        [ $RETVAL -eq 0 ]
+        echo
+        [ $RETVAL -eq 0 ] && touch $SLOCK
+        return $RETVAL
 }
 do_stop()
 {
-        echo -n $"Stopping PhantomJS : "
-        pid=`ps -aefw | grep "$DAEMON" | grep -v " grep " | awk '{print $2}'`
-        kill -9 $pid > /dev/null 2>&1 && echo_success || echo_failure
-        RETVAL=$?
-        echo
-        [ $RETVAL -eq 0 ]
+        # Stop PhantomJS
+        if [ -e "$PID" ]; then
+            echo -n $"Stopping PhantomJS: "
+            killproc -p $PID
+            RETVAL=$?
+            echo
+            [ $RETVAL -eq 0 ] && rm -f $SLOCK
+            return $RETVAL
+        else
+            echo "Error! PhantomJS server is not running!" 1>&2
+            exit 1
+        fi
+}
+get_status() {
+    status -p $PID phantomjs
 }
 
 case "$1" in
@@ -40,8 +53,11 @@ case "$1" in
                 do_stop
                 do_start
                 ;;
+        status)
+                get_status
+                ;;
         *)
-                echo "Usage: $0 {start|stop|restart}"
+                echo "Usage: $0 {start|stop|restart|status}"
                 RETVAL=1
 esac
 
